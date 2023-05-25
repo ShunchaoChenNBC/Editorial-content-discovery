670d7a6acece00b3f87de75d98ab4b3b84dffb71
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-05-25 16:39:49 -0400

    Update Editorial_Content_Top_50.sql

--- Editorial_Content_Top_50.sql
+++ Editorial_Content_Top_50.sql
@@ -1,27 +1,40 @@
-create or replace table `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Top_50_Contents` as
+create or replace table `nbcu-ds-sandbox-a-001.Shunchao_Sandbox.Top_Daily_30_Contents` as
 
-with cte as (select 
-DATE_ADD(DATE_TRUNC(adobe_date, WEEK(SUNDAY)), interval -7 DAY) as Week_Ending, --Sunday as the last day of a Week
-lower(display_name) as Display_Name,
-count(distinct adobe_tracking_id) as Distinct_Accts,
-from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
-where num_seconds_played_no_ads> 300 and adobe_date between "2023-05-01" and "2023-05-20" and lower(consumption_type_detail) = "vod"
-group by 1,2), -- 5 minute qualifier 
-
-cte1 as (select cte.*,
-dense_rank() over (partition by Week_Ending order by Distinct_Accts desc) as Ranks
-from cte)
-
+with D1 as (
 select 
-Week_Ending,
-Display_Name,
-Distinct_Accts,
-Ranks,
-Distinct_Accts - LAG(Distinct_Accts) over (partition by Display_Name order by Week_Ending) as Incremental_Reached,
-sum(Distinct_Accts) over (partition by Display_Name order by Week_Ending) as Cumulative_Reached
-from cte1
-where Ranks <= 50
-order by 1 desc,4
+current_date("America/New_York")-2 as Days,
+lower(display_name) as Display_Name,
+count(distinct adobe_tracking_id) as Cumul_reached,
+from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
+where 1=1
+and adobe_date between DATE_ADD(current_date("America/New_York")-2, INTERVAL -18*30 DAY) and current_date("America/New_York")-2
+and num_seconds_played_no_ads > 300
+and lower(consumption_type_detail) = "vod" -- VOD top 30
+group by 1,2
+order by 3 desc
+limit 30
+),
+
+D2 as (
+select
+current_date("America/New_York")-3 as Days,
+lower(display_name) as Display_Name,
+count(distinct adobe_tracking_id) as Cumul_reached,
+from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
+where 1=1
+and adobe_date between DATE_ADD(current_date("America/New_York")-2, INTERVAL -18*30 DAY) and current_date("America/New_York")-3
+and num_seconds_played_no_ads > 300
+and lower(display_name) in (select distinct Display_Name from D1)
+and lower(consumption_type_detail) = "vod"
+group by 1,2
+order by 3 desc
+)
+
+select D1.*,
+D1.Cumul_reached - D2.Cumul_reached as Net_New_Reached
+from D1
+left join D2 on D1.Display_Name = D2.Display_Name
+
 
 
 
