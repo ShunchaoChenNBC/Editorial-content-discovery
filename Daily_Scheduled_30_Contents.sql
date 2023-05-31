9451a216af48ba0a37bc81401712ed6042a8c5b3
ShunchaoChenNBC <117398619+ShunchaoChenNBC@users.noreply.github.com>
2023-05-31 10:45:21 -0400

    Update Daily_Scheduled_30_Contents.sql

--- Daily_Scheduled_30_Contents.sql
+++ Daily_Scheduled_30_Contents.sql
@@ -1,5 +1,5 @@
 
-with Top_30_Contents as ( ---Pull top 30 VOD display_name by daily qualified reach on 5/24 (> 5 minutes watched)
+with Top_50_Contents as ( ---Pull top 30 VOD display_name by daily qualified reach on 5/24 (> 5 minutes watched)
 select 
 current_date("America/New_York")-1 as Days,
 lower(display_name) as Display_Name,
@@ -8,10 +8,10 @@ from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
 where 1=1
 and adobe_date = current_date("America/New_York")-1
 and num_seconds_played_no_ads > 300
-and lower(consumption_type_detail) = "vod" -- VOD top 30
+and lower(consumption_type_detail) = "vod" -- VOD top 50
 group by 1,2
 order by 3 desc
-limit 30
+limit 50
 ),
 
 D1 as (
@@ -23,7 +23,7 @@ from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
 where 1=1
 and adobe_date between DATE_ADD(current_date("America/New_York")-1, INTERVAL -18*30 DAY) and current_date("America/New_York")-1
 and num_seconds_played_no_ads > 300
-and lower(s.display_name) in (select distinct Display_Name from Top_30_Contents)
+and lower(s.display_name) in (select distinct Display_Name from Top_50_Contents)
 and lower(consumption_type_detail) = "vod" -- VOD top 30
 group by 1,2
 order by 3 desc
@@ -38,7 +38,7 @@ from `nbcu-ds-prod-001.PeacockDataMartSilver.SILVER_VIDEO` s
 where 1=1
 and adobe_date between DATE_ADD(current_date("America/New_York")-1, INTERVAL -18*30 DAY) and current_date("America/New_York")-2
 and num_seconds_played_no_ads > 300
-and lower(display_name) in (select distinct Display_Name from Top_30_Contents)
+and lower(display_name) in (select distinct Display_Name from Top_50_Contents)
 and lower(consumption_type_detail) = "vod"
 group by 1,2
 order by 3 desc
@@ -51,5 +51,5 @@ D1.Cumul_Reached,
 D1.Cumul_reached - D2.Cumul_reached as Net_New_Reached
 from D1
 left join D2 on D1.Display_Name = D2.Display_Name
-left join Top_30_Contents t on t.Display_Name = D1.Display_Name
+left join Top_50_Contents t on t.Display_Name = D1.Display_Name
 order by 4 desc
